a5a48a285f5e4bb165693fe1ab59f7d6a537c0cd
haja171106 <hei.haja.2@gmail.com>
2026-07-29 19:22:44 +0300

    test: add env and sqlite test configuration

---
+++ src/test/java/com/school/luc/conf/FacadeITMockedThirdParties.java
@@ -0,0 +1,15 @@
+﻿package com.school.luc.conf;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import com.school.luc.endpoint.event.EventProducer;
+import com.school.luc.file.bucket.BucketComponent;
+
+@AutoConfigureMockMvc
+public class FacadeITMockedThirdParties extends FacadeIT {
+  @LocalServerPort protected int localPort;
+  @MockBean protected BucketComponent bucketConfMock;
+
+  @MockBean protected EventProducer eventProducerMock;
+}
---
+++ src/test/java/com/school/luc/conf/SqliteConf.java
@@ -0,0 +1,15 @@
+﻿package com.school.luc.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import com.school.luc.PojaGenerated;
+
+@PojaGenerated
+public class SqliteConf {
+
+  void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("driverClassName", () -> "org.sqlite.JDBC");
+    registry.add("spring.datasource.url", () -> "jdbc:sqlite:/tmp/sqlite-data:db?cache=shared");
+    registry.add("spring.datasource.username", () -> "sa");
+    registry.add("spring.datasource.password", () -> "sa");
+  }
+}
