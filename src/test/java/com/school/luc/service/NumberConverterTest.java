a7e51ad70e9a3f6f39b191a49343b559c9fe5af5
haja171106 <hei.haja.2@gmail.com>
2026-07-29 19:23:04 +0300

    test(model): add unit tests for domain models and utilities

---
+++ src/test/java/com/school/luc/service/NumberConverterTest.java
@@ -0,0 +1,37 @@
+﻿package com.school.luc.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import com.school.luc.number.NumberConverter;
+
+public class NumberConverterTest {
+
+  @Test
+  void amount_with_currency() {
+    var numberConverter = new NumberConverter();
+    var amount = "180 000";
+    assertEquals("Cent quatre-vingt mille", numberConverter.convertToWords(amount));
+  }
+
+  @Test
+  void large_amount_with_currency() {
+    var numberConverter = new NumberConverter();
+    var amount = "2 500 000";
+    assertEquals("Deux millions cinq cent mille", numberConverter.convertToWords(amount));
+  }
+
+  @Test
+  void amount_without_currency() {
+    var numberConverter = new NumberConverter();
+    var amount = "1000";
+    assertEquals("Mille", numberConverter.convertToWords(amount));
+  }
+
+  @Test
+  void amount_with_decimal() {
+    var numberConverter = new NumberConverter();
+    var amount = "1101,25";
+    assertEquals("Mille cent un", numberConverter.convertToWords(amount));
+  }
+}
