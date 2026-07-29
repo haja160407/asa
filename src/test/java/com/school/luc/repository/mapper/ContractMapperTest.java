f835b345284ffab9a6a98c3cbc393d0c50a98fad
haja171106 <hei.haja.2@gmail.com>
2026-07-29 19:24:28 +0300

    test(repository): add repository and mapper tests

---
+++ src/test/java/com/school/luc/repository/mapper/ContractMapperTest.java
@@ -0,0 +1,96 @@
+﻿package com.school.luc.repository.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static school.hei.asa.model.contract.ContractType.studentContractor;
+
+import java.time.Duration;
+import java.time.Instant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.school.luc.model.DailyExecution;
+import com.school.luc.model.Worker;
+import com.school.luc.model.contract.Contract;
+import com.school.luc.model.contract.ContractLevel;
+import com.school.luc.repository.DailyExecutionRepository;
+import com.school.luc.repository.model.JContract;
+import com.school.luc.repository.model.JContractLevel;
+import com.school.luc.repository.model.JWorker;
+
+public class ContractMapperTest {
+
+  private DailyExecutionRepository dailyExecutionRepository;
+  private ContractMapper contractMapper;
+
+  @BeforeEach
+  void setUp() {
+    contractMapper = new ContractMapper(new WorkerMapper(), new ContractLevelMapper());
+  }
+
+  @Test
+  void mapping_to_domain() {
+    var w = newWorker();
+    var de = mock(DailyExecution.class);
+    var expected = newModel(w, de);
+    var jContract = newEntity();
+
+    var actual = contractMapper.toDomain(jContract, new Cache());
+
+    assertEquals(expected, actual);
+  }
+
+  private JContract newEntity() {
+    JContract jContract = new JContract();
+    jContract.setId("id");
+    jContract.setWorker(newJWorker());
+    jContract.setLevel(newJWorkerLevel());
+    jContract.setEntranceInstant(newInstant());
+
+    var jLevel = new JContractLevel();
+    jLevel.setType(studentContractor);
+    jLevel.setDailyPay(50_000d);
+    jLevel.setCode("level");
+    jContract.setLevel(jLevel);
+
+    jContract.setJobTitle("job title");
+    jContract.setDurationInDays(100);
+    jContract.setContractBucketKey("DUMMY_BUCKET");
+
+    return jContract;
+  }
+
+  private Contract newModel(Worker w, DailyExecution de) {
+    var level = new ContractLevel("level", studentContractor, null, 50_000.);
+    return new Contract(
+        w, "job title", level, newInstant(), null, Duration.ofDays(100), null, "DUMMY_BUCKET");
+  }
+
+  private JWorker newJWorker() {
+    JWorker jWorker = new JWorker();
+    jWorker.setCode("code");
+    jWorker.setName("code");
+    jWorker.setEmail("email");
+    jWorker.setFullname("fullname");
+    jWorker.setAddress("address");
+    jWorker.setCity("city");
+    jWorker.setNif("NIF");
+    jWorker.setStat("STAT");
+
+    return jWorker;
+  }
+
+  private Worker newWorker() {
+    return new Worker("code", "code", "email", "fullname", "address", "city", "NIF", "STAT");
+  }
+
+  private JContractLevel newJWorkerLevel() {
+    JContractLevel jContractLevel = new JContractLevel();
+    jContractLevel.setCode("level");
+
+    return jContractLevel;
+  }
+
+  private Instant newInstant() {
+    return Instant.ofEpochSecond(1735689600);
+  }
+}
