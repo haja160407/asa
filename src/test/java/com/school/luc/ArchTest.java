7b7a2c84b562db644d7c9c8edc79fa523ad673d9
haja171106 <hei.haja.2@gmail.com>
2026-07-29 19:22:56 +0300

    test: add architecture tests

---
+++ src/test/java/com/school/luc/ArchTest.java
@@ -0,0 +1,19 @@
+﻿package com.school.luc;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "com.school.luc")
+public class ArchTest {
+
+  @com.tngtech.archunit.junit.ArchTest
+  static final ArchRule rest_is_used_by_rest_only =
+      noClasses()
+          .that()
+          .resideOutsideOfPackage("..rest..")
+          .should()
+          .dependOnClassesThat()
+          .resideInAPackage("..rest..");
+}
