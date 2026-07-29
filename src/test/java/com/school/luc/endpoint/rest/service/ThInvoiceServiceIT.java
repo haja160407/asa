648303dd7b62b374384cd226d6ff47409e01352b
haja171106 <hei.haja.2@gmail.com>
2026-07-29 19:24:36 +0300

    test(service): add service unit and integration tests

---
+++ src/test/java/com/school/luc/endpoint/rest/service/ThInvoiceServiceIT.java
@@ -0,0 +1,27 @@
+﻿package com.school.luc.endpoint.rest.service;
+
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.school.luc.conf.FacadeIT;
+import com.school.luc.endpoint.event.EventProducer;
+import com.school.luc.endpoint.event.model.NewInvoiceGenerated;
+import com.school.luc.service.InvoiceService;
+
+public class ThInvoiceServiceIT extends FacadeIT {
+  @Autowired ThInvoiceService thInvoiceService;
+  @MockBean EventProducer<NewInvoiceGenerated> eventProducer;
+  @Autowired private InvoiceService invoiceService;
+
+  @Test
+  void send_invoice_copy_ok() throws IOException {
+    var event = mock(NewInvoiceGenerated.class);
+    doNothing().when(eventProducer).accept(List.of(event));
+    invoiceService.sendGenerateInvoiceEvent("invoiceId");
+    verify(eventProducer).accept(anyList());
+  }
+}
